b297ee8753feb13c4f1bd51c719ab9865efbe371
Maksim Kanev <maks_kv@yahoo.com>
2019-09-30 22:33:46 +0300

    Added a flight route model

---
+++ FlightRoutesCalculator/Models/FlightRouteModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightRoutesCalculator.Models
+{
+    public class FlightRouteModel
+    {
+        public string ID { get; set; }
+        public string Airline { get; set; }
+        public string DepartureAirportCode { get; set; }
+        public string ArrivalAirportCode { get; set; }
+        public int Price { get; set; }
+
+        public FlightRouteModel(string id, string airline, string departureCode, string arrivalCode, int price) 
+        {
+            ID = id;
+            Airline = airline;
+            DepartureAirportCode = departureCode;
+            ArrivalAirportCode = arrivalCode;
+            Price = price;
+        }
+    }
+}
